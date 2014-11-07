4d2d028b0a3e867bdab802374ef366643daa6ec1
Yann <me.show@gmail.com>
2014-11-07 17:33:52 +0100

    commit all

---
+++ FormsBasics.Android/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Forms2Native
+{
+	/// <summary>
+	/// Android app starts with Xamarin.Forms, then opens a natively rendered Page
+	/// </summary>
+	[Activity (Label = "Forms2Native", MainLauncher = true)]
+	public class Activity1 : Xamarin.Forms.Platform.Android.AndroidActivity
+	{
+		public static Page page;
+		public static Xamarin.Forms.Platform.Android.AndroidActivity test;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			test = this;
+			Console.WriteLine ("created " + DateTime.Now);
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			// Starts with the main Xamarin.Forms screen
+			page = App.GetMainPage ();
+			SetPage (page);
+
+			MyFirstPage.test = () => {
+				//var activity = this.Context as Activity;
+				var thirdActivity = new Intent (this, typeof(MyThirdActivity));
+				StartActivity (thirdActivity);
+			};
+		}
+
+		protected override void OnPostResume ()
+		{
+			base.OnPostResume ();
+
+			//SetPage (page);
+		}
+
+		public override void OnBackPressed ()
+		{
+			Console.WriteLine ("back pressed " + DateTime.Now);
+			base.OnBackPressed ();
+		}
+	}
+}
+
+
---
+++ FormsBasics.Android/MyThirdActivity.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content.PM;
+using Xamarin.Forms.Platform.Android;
+using QuickTodo;
+using Xamarin.Forms;
+using System.Reflection;
+
+namespace Forms2Native
+{
+	[Activity (Label = "MyThirdActivity",
+		ConfigurationChanges = ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout)]
+	public class MyThirdActivity : AndroidActivity
+	{
+		public static Page page = null;
+		public static Xamarin.Forms.Platform.Android.AndroidActivity test;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			test = this;
+			/*RequestWindowFeature(WindowFeatures.ActionBar);
+			RequestWindowFeature(WindowFeatures.ActionBarOverlay);*/
+
+			base.OnCreate (bundle);
+
+			page = new NavigationPage(new MyPage ());
+			this.SetPage (page);
+		}
+
+		/* Uncomment this code to make this work 
+		public override void OnBackPressed ()
+		{
+			var ev = typeof(AndroidActivity).GetField ("BackPressed", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+			var list = (Delegate[])ev.GetType ().GetMethod ("GetInvocationList").Invoke (ev, null);
+			var test = (BackButtonPressedEventHandler)list.Last();
+			AndroidActivity.BackPressed -= test;
+			this.Finish ();
+		}
+		*/
+	}
+}
+
